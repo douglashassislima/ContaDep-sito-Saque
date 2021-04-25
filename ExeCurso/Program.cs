6fb3606831919f3d388f1f77ad0fe272dbb6c17d
douglashassislima <douglashenriquedeassis@gmail.com>
2021-04-25 20:33:42 -0300

    iniciando projeto

---
+++ ExeCurso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExeCurso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
