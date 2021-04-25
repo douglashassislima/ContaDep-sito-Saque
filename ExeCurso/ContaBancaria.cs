66c934d04b314957080f9dc2a0b76c18a776af8a
douglashassislima <douglashenriquedeassis@gmail.com>
2021-04-25 20:38:15 -0300

    criando props

--- ExeCurso/ContaBancaria.cs
+++ ExeCurso/ContaBancaria.cs
@@ -6,5 +6,11 @@ namespace ExeCurso
 {
     class ContaBancaria
     {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+
+
     }
 }
